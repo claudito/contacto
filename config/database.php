2426871802b47ca1e19f81db7dda4cbf8954bf8a
claudito <1107000044@upci.edu.pe>
2019-03-12 01:04:17 -0500

    first commit

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php 
+
+set_time_limit(5000);
+define("URL","http://localhost/admin2/");
+date_default_timezone_set('America/Lima');
+define("KEY","adecb2e24eb1c1f24edd097e7aba7967-admin2/");
+define("FOLDER","/admin2/");
+define("SERVER", "200.60.144.12");
+define("USER", "root");
+define("PASS", "csglobal2019");
+//define("PASS", "mochiladb");
+define("BD", "db_controlnet");
+define("ID", "ID");
+define("NOMBRES", "NOMBRES");
+define("APELLIDOS", "APELLIDOS");
+
+/*
+CONFIG MAIL
+*/
+define("USER_MAIL", "envio.mail.sistemas@gmail.com");
+define("USER_PASS","mochilanegra");
+
+
+
+ ?>
